9bc289a0289350cd3616350f5014ac4105770c6b
Andrei Tufeanu <andrei.tufeanu13@gmail.com>
2025-01-11 20:24:37 +0200

    feat: add IClientSDK and IClientSDKListener interfaces

---
+++ ClientSDK/IClientSDK.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "IGameConfig.h"
+#include "EPieceType.h"
+#include "IClientSDKListener.h"
+
+#include <functional>
+
+using NodesInfo = std::vector<std::pair<int, EPieceType>>;
+
+using IClientSDKPtr = std::shared_ptr<class IClientSDK>;
+
+class IClientSDK
+{
+public:
+	static IClientSDKPtr Create(std::string ip, int port);
+
+	virtual void SetListener(IClientSDKListener* listener) = 0;
+
+	//requests
+	virtual void CreateLobby() = 0;
+	virtual void JoinLobby(int id) = 0;
+
+	virtual void StartGame(GameConfigPtr config) = 0;
+
+	virtual void SetupBoard() = 0;
+
+	virtual void PlacePiece(uint8_t index) = 0;
+	virtual void RemovePiece(uint8_t index) = 0;
+	virtual void MovePiece(uint8_t from, uint8_t to) = 0;
+	virtual void Undo() = 0;
+
+	virtual void GetTimer() = 0;
+	virtual void GetPossibleMoves(uint8_t index) = 0;
+	virtual void GetPossibleRemoves() = 0;
+	virtual void GetActivePlayer() = 0;
+};
---
+++ ClientSDK/IClientSDKListener.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "EBoardType.h"
+#include "EBoardSize.h"
+#include "EGameState.h"
+#include "EPieceType.h"
+
+#include <vector>
+#include <string>
+
+using NodesInfo = std::vector<std::pair<int, EPieceType>>;
+using PiecesIndexes = std::vector<uint8_t>;
+
+class IClientSDKListener
+{
+public:
+	virtual void OnCreatedLobby(int lobbyId) = 0;
+	virtual void OnJoinedLobby(int lobbyId) = 0;
+
+	virtual void OnGameStarted() = 0;
+
+	virtual void OnInfo(const std::string& message) = 0;
+	virtual void OnSetupBoard(NodesInfo nodesInfo) = 0;
+	virtual void OnPlayerLeft() = 0;
+	virtual void OnUpdateTimer(int timeRemaining) = 0;
+	virtual void OnWinningPlayer(const std::string& player) = 0;
+	virtual void OnAddedPiece(int index, EPieceType pieceType) = 0;
+	virtual void OnMovedPiece(int from, int to, EPieceType pieceType) = 0;
+	virtual void OnRemovedPiece(int index) = 0;
+	virtual void OnPlayerRemoved(const std::string& player) = 0;
+	virtual void OnPlayerChanged(const std::string& player, bool isComputer) = 0;
+	virtual void OnStateChanged(EGameState state) = 0;
+	virtual void OnPossibleMoves(PiecesIndexes indexes) = 0;
+	virtual void OnPossibleRemoves(PiecesIndexes indexes) = 0;
+
+	virtual void OnServerDisconnect() = 0;
+};
